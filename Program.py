555a2f56d831042d4903df66b5546bc47cef5e31
thekatiedee <72450666+thekatiedee@users.noreply.github.com>
2021-05-10 00:52:14 -0400

    Update Program.py

--- Program.py
+++ Program.py
@@ -19,41 +19,6 @@ def main():
     the_car.brake()
     print('Your decelerated speed is ', the_car.get_speed())
     
-  
-    
-# def theAccelerate():
-    
-    #i = 0
-    
-    #while i <= 4:
-        #print(the_car.accelerate())
-        #i += 1
-        #print(i)
-    
-#def theBrake():
-    
-    #d = 4
-    
-    #while d >= 0:
-        #print(the_car.brake())
-        #d -= 1
-        #print(d)
-        
 
 main()
-
-# def theLoops():
-    
-    # i = 0
-    # d = 25
-
-    # while i <= 25:
-        # print(i)
-        # i += 5
-        
-
-    # while d >= 25:
-        # print(d)
-        # d -= 5
-    
 
